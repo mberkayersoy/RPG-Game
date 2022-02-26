aee977c362145d200d72d0dab7a4953edf693ad6
mberkayersoy <mberkayersoy@gmail.com>
2022-02-26 20:54:53 +0300

    Added a cooldown between the player's attacks.

--- RPG-GAME/Assets/Scripts/Combat/Fighter.cs
+++ RPG-GAME/Assets/Scripts/Combat/Fighter.cs
@@ -10,9 +10,13 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2.0f;
+        [SerializeField] float timeBetweenAttacks = 1f;
         Transform target;
+        float timeSinceLastAttack = 0;
         private void Update() 
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
             
             if (target != null && !GetIsInRange())
@@ -22,7 +26,18 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            if(timeSinceLastAttack > timeBetweenAttacks) 
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+            
         }
 
         private bool GetIsInRange()
@@ -39,6 +54,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation Event
+        void Hit()
+        {
+
         }
     }
 }
