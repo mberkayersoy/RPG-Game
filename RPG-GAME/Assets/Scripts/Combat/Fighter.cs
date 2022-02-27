676712bf809a2646c53b431b04199c29b1570283
mberkayersoy <mberkayersoy@gmail.com>
2022-02-27 19:37:34 +0300

    stop attacking
    Changed the target's type from transform to Health in the PlayerController class.
    The player no longer continues to attack when the target dies.

--- RPG-GAME/Assets/Scripts/Combat/Fighter.cs
+++ RPG-GAME/Assets/Scripts/Combat/Fighter.cs
@@ -13,7 +13,8 @@ namespace RPG.Combat
         [SerializeField] float timeBetweenAttacks = 1f;
         //[SerializeField] Health enemyHealth; enemy take damage my way.
         [SerializeField] float weaponDamage = 5;
-        Transform target;
+        Health target; // It used to be transformed, but we changed it to access the health class more easily. 
+                          //Since every enemy has to have health, it will not cause much trouble.
         float timeSinceLastAttack = 0;
         
         private void Update() 
@@ -21,10 +22,11 @@ namespace RPG.Combat
             timeSinceLastAttack += Time.deltaTime;
 
             if (target == null) return;
+            if (target.IsDead()) return;
             
             if (target != null && !GetIsInRange())
             {
-                GetComponent<Mover>().MoveTo(target.position );
+                GetComponent<Mover>().MoveTo(target.transform.position );
             }
             else
             {
@@ -51,23 +53,23 @@ namespace RPG.Combat
         //If we look at the attack animation, we can see the key of this method in about the 11th frame.
         void Hit()
         {
-            Health healthComponent = target.GetComponent<Health>();
-            healthComponent.TakeDamage(weaponDamage);        
+            target.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
         {
-            return Vector3.Distance(transform.position, target.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            target = combatTarget.transform;
+            target = combatTarget.GetComponent<Health>();
         }
 
         public void Cancel()
         {
+            GetComponent<Animator>().SetTrigger("stopAttack"); 
             target = null;
         }
     }
--- RPG-GAME/Assets/Scripts/Combat/Health.cs
+++ RPG-GAME/Assets/Scripts/Combat/Health.cs
@@ -10,6 +10,11 @@ namespace RPG.Combat
         [SerializeField] float healthPoints = 100f;
         bool isDead = false;
 
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
         public void TakeDamage(float damage)
         {
             healthPoints = Mathf.Max(healthPoints - damage, 0);
