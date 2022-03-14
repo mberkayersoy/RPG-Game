6882d942862ea0464c68e0e440956bf3f2b03e4b
mberkayersoy <mberkayersoy@gmail.com>
2022-03-14 14:39:36 +0300

    portal added. particle effect is used for portal view.Added transition between portals. Matched portals to each other using enum

--- RPG-GAME/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG-GAME/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,15 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class Portal : MonoBehaviour
 {
+    enum DestinationIdentifier
+    {
+        A, B, C, D, E
+    }
     [SerializeField] int sceneToLoad = -1;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] DestinationIdentifier destination;
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(Transition());
         }
+    }
+
+    private IEnumerator Transition()
+    {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+        Portal otherPortal = GetOtherPortal();
+        UpdatePlayer(otherPortal);
+        Destroy(gameObject, 2f);
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+
+        GameObject player = GameObject.FindWithTag("Player");
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+        player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private Portal GetOtherPortal()
+    {
+        foreach (Portal portal in FindObjectsOfType<Portal>())
+        {
+            if (portal == this) continue;
+            if (portal.destination != destination) continue;
+
+            return portal;
+        }
+        return null;
     }
 }
