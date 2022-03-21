076d692829d5ae814a5cc7e94808fc8756ee90a1
mberkayersoy <mberkayersoy@gmail.com>
2022-03-21 16:09:33 +0300

    Resetting the default Animator.

--- RPG-GAME/Assets/Scripts/Combat/Weapon.cs
+++ RPG-GAME/Assets/Scripts/Combat/Weapon.cs
@@ -29,9 +29,15 @@ namespace RPG.Combat
                 GameObject weapon = Instantiate(equippedPrefab, handTransform);
                 weapon.name = weaponName;
             }
+            
+            var overrideController = animator.runtimeAnimatorController as AnimatorOverrideController;
             if (animatorOverride != null)
             {
                 animator.runtimeAnimatorController = animatorOverride;
+            }
+            else if (overrideController != null)
+            {
+                    animator.runtimeAnimatorController = overrideController.runtimeAnimatorController;
             }
         }
 
