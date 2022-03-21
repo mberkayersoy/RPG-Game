39f5bb515ccc8c2e48471dcb9e8a038ea1a70e2c
mberkayersoy <mberkayersoy@gmail.com>
2022-03-21 20:49:43 +0300

    Added Progression Design for character stats.

---
+++ RPG-GAME/Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats{
+
+
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1, 99)]
+        [SerializeField] int startingLevel = 1;
+        [SerializeField] CharacterClass characterClass;
+    }
+}
