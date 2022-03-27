d9c98b492bf9455a4872845e631579ae490ba7ec
mberkayersoy <mberkayersoy@gmail.com>
2022-03-27 21:23:27 +0300

    Experience Display and Performans lookups with dictionaries.

--- RPG-GAME/Assets/Scripts/Attributes/Experience.cs
+++ RPG-GAME/Assets/Scripts/Attributes/Experience.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Attributes
 {
-    public class Experience : MonoBehaviour 
+    public class Experience : MonoBehaviour, ISaveable
     {
-        [SerializeField] float experiencePoints = 10;
+        [SerializeField] float experiencePoints = 0;
 
         public void GameExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public float GetExperience()
+        {
+            return experiencePoints;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 }
---
+++ RPG-GAME/Assets/Scripts/Attributes/ExperienceDisplay.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Attributes
+{
+
+    public class ExperienceDisplay : MonoBehaviour
+    {
+        Experience experience;
+
+        private void Awake() 
+        {
+            experience = GameObject.FindWithTag("Player").GetComponent<Experience>();    
+        }
+
+        private void Update() 
+        {
+            GetComponent<Text>().text = String.Format("{0:0}", experience.GetExperience());
+        }
+    }
+}
